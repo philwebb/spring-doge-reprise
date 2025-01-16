0d422fd72660c321c3c2298efccd662b756a2c0d
Phillip Webb <phil.webb@broadcom.com>
2025-01-16 13:26:26 -0800

    Use @SpringBootApplication
    Nice new annotation!

--- spring-doge/src/main/java/doge/DogeApplication.java
+++ spring-doge/src/main/java/doge/DogeApplication.java
@@ -23,15 +23,11 @@ import doge.photo.DogePhotoManipulator;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableConfigurationProperties(DogeProperties.class)
 public class DogeApplication {
 
